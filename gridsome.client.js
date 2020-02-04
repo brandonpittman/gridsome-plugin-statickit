bf1d175afaa656c349efe4034dd2d5e50edf2244
Brandon Pittman <brandon@brandonpittman.net>
2020-02-04 15:27:04 +0900

    wip

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,3 +1,33 @@
+// import {createClient} from '@statickit/core'
+//
+// export default function (Vue, options, {isClient}) {
+//   const client = createClient({site: options.site})
+//
+//   Vue.prototype.$createStatickitForm = function () {}
+//
+//   if (!isClient) return
+//
+//   Vue.prototype.$createStatickitForm = async function (fnOptions) {
+//     let {form, element} = fnOptions
+//     client.startBrowserSession()
+//
+//     element.addEventListener('submit', async event => {
+//       event.preventDefault()
+//       const formData = new FormData(event.target);
+//       const {body, response} = await client.submitForm(form, formData);
+//       console.log(body)
+//       console.log(response)
+//     })
+//
+//     function onSuccess() {}
+//     function onError() {}
+//
+//     this.$once("hook:beforeDestroy", () => {
+//       client.teardown()
+//     })
+//   }
+// }
+
 export default function (Vue, options, {isClient}) {
   const {site} = options
 
